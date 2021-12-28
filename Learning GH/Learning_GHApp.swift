a7a70fd388cd5316553632b34e11fa27c5a01b94
Dag Tolstoy <dag.tolstoy@gmail.com>
2021-12-28 17:23:23 +0100

    Initial Commit

---
+++ Learning GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning GH
+//
+//  Created by Dag Tolstoy on 2021-12-28.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
